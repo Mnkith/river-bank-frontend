e57bd420149e4714b30b4e5107329397b61efcbf
Mnkith <munkithabid@gmail.com>
2021-08-21 19:07:14 -0500

    add Account compo

--- src/containers/AccountsList.js
+++ src/containers/AccountsList.js
@@ -1,2 +1,39 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
 
+const AccountsList = (props) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Account Type</th>
+          <th>Current Ballance</th>
+          <th>Account Number</th>
+          <th>Expiration Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  )
+}
+
+export default AccountsList
